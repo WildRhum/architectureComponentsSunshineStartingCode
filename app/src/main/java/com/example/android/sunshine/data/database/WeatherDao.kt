23b316fdd99501a62dbbebb6dfe5917b4413fe72
mrome <kyon-04@hotmail.fr>
2018-07-20 15:58:01 +0200

    En cours, a terminer : https://codelabs.developers.google.com/codelabs/build-app-with-arch-components/index.html?index=..%2F..%2Findex#6

---
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherDao.kt
@@ -0,0 +1,19 @@
+package com.example.android.sunshine.data.database
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.Date;
+
+@Dao
+interface WeatherDao  {
+
+    // Checked at compile time and not real time !
+    @Query("SELECT * FROM weather WHERE date = :date")
+    fun getWeatherByDate(date: Date): WeatherEntry
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void bulkInsert(WeatherEntry... weather);
+}
